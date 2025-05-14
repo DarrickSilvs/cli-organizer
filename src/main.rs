41496eb3c4fd045568a3c199989f363013160a51
Darrick <darricksilvester@gmail.com>
2025-05-14 22:33:18 +1000

    Read directory implementation

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,26 @@ struct Cli {
 }
 fn main() {
     let args = Cli::parse();
+    let entries = std::fs::read_dir(&args.path)
+        .expect("Could not read directory");
 
-    println!("extension: {:?}, path: {:?}", args.f_ext, args.path);
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+
+            if path.is_file() {
+                // check file extension
+                if let Some(ext) = &args.f_ext {
+                    if let Some(extension) = path.extension() {
+                        if ext == extension.to_str().unwrap() {
+                            println!("Matched file: {:#?}", path);
+                        }
+                    }
+                } else {
+                    // no extension given
+                    println!("{:?}", path);
+                }
+            }
+        }
+    }
 }
