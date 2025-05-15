d53931135976976a0ed699896349cedc1603d483
Darrick <darricksilvester@gmail.com>
2025-05-15 12:07:39 +1000

    sorting logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use clap::Parser;
 use anyhow::{Context, Result};
 
@@ -15,6 +17,25 @@ fn main() -> Result<()> {
     let entries = std::fs::read_dir(&args.path)
         .with_context(|| format!("Could not read directory `{}`", args.path.display()))?;
 
+    let mut folders: HashMap<String, Vec<std::path::PathBuf>> = HashMap::new();
+
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+
+            if path.is_dir() {
+                // Check for existing folders
+                if let Some(name) = path.file_name()
+                    .and_then(|os_str| os_str.to_str()) 
+                {
+                    folders.insert(name.to_string(), Vec::new());
+                }
+            }
+        }
+    }
+
+    let entries = std::fs::read_dir(&args.path)
+        .with_context(|| format!("Could not read directory `{}`", args.path.display()))?;
 
     for entry in entries {
         if let Ok(entry) = entry {
@@ -26,11 +47,24 @@ fn main() -> Result<()> {
                     if let Some(extension) = path.extension() {
                         if ext == extension.to_str().unwrap() {
                             println!("Matched file: {:#?}", path);
+
+                            let folder_name = format!("{}_files", ext);
+                            folders
+                                .entry(folder_name)
+                                .or_insert_with(|| vec![])
+                                .push(path);
                         }
                     }
                 } else {
                     // no extension given
                     println!("{:?}", path);
+                    if let Some(extension) = path.extension() {
+                        let folder_name = format!("{}", extension.to_str().unwrap());
+                        folders
+                                .entry(folder_name)
+                                .or_insert_with(|| vec![])
+                                .push(path);
+                    }
                 }
             }
         }
