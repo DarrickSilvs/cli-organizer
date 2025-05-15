5c56980ec77e05aa37524b21fa62b32e22af0a96
Darrick <darricksilvester@gmail.com>
2025-05-15 16:47:31 +1000

    Usage Details

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,12 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
-#[command(name = "cli-organizer", version, about = "Organizes files by extension")]
+#[command(
+    name = "cli-organizer",
+    version,
+    about = "Organizes files by extension",
+    long_about = "Usage during development: cargo run -- <PATH> [F_EXT]\n\nUsage after install: cli-organizer <PATH> [F_EXT]"
+)]
 pub struct Cli {
     /// Path to the folder to organize
     pub path: PathBuf,
